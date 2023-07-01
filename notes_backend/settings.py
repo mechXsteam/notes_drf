af0223a380895381564196e446e1f6527e697149
Aditya Kumar <adityakumar_me20a8_36@dtu.ac.in>
2023-07-02 03:17:29 +0530

    Initial commit

--- notes_backend/settings.py
+++ notes_backend/settings.py
@@ -154,4 +154,8 @@ REST_FRAMEWORK = {
 # allowing all the different domains to access my api
 CORS_ALLOW_ALL_ORIGINS: True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://djnotes.netlify.app/",
+]
+
 REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
